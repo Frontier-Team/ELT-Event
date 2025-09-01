582ea899857e36747da14db0bad10ec9596723a1
Rose <rose.ulldemolins@live.co.uk>
2025-09-01 16:32:05 +0100

    removed unused title

--- src/pages/AuctionPage.tsx
+++ src/pages/AuctionPage.tsx
@@ -1,33 +1,31 @@
-import { Portfolio } from '../types';
-import data from '../db.json';
-import { Table } from '../components/Table';
-import { Heading, PageContainer, Paragraph } from '../styles/sharedStyles';
+import { Portfolio } from "../types";
+import data from "../db.json";
+import { Table } from "../components/Table";
+import { Heading, PageContainer, Paragraph } from "../styles/sharedStyles";
 
 export const AuctionPage = () => {
-  const headers = [
-    'Name',
-    'Occupation',
-    'Details',
-    'Get in touch',
-  ];
+  const headers = ["Name", "Occupation", "Details", "Get in touch"];
 
   const fieldsToDisplay: (keyof Portfolio)[] = [
-    'name',
-    'occupation',
-    'details',
-    'contact',
+    "name",
+    "occupation",
+    "details",
+    "contact",
   ];
 
   return (
     <PageContainer>
       <Heading>Auction</Heading>
-      <Paragraph>During the event there will be an auction of several pieces from a select group of artists. All proceeds of the auction will go to support the V&A Museum.</Paragraph>
+      <Paragraph>
+        During the event there will be an auction of several pieces from a
+        select group of artists. All proceeds of the auction will go to support
+        the V&A Museum.
+      </Paragraph>
       <Table
-        heading='Here are some details of the artists we are showcasing.'
+        heading="Here are some details of the artists we are showcasing."
         headers={headers}
         rows={data.auctionWorks}
         fieldsToDisplay={fieldsToDisplay}
-        largeFields={['contact']}
       />
     </PageContainer>
   );
